a902cc103ee9b29fc96650d2ab6a3f0bd9d18594
GrandDukeShun <hiroseshun2000@gmail.com>
2018-02-22 21:14:57 +0900

    jQueryのテスト

--- js/load.js
+++ js/load.js
@@ -1,17 +1,21 @@
-$(function() {
-	$.ajax('../html/CHUNITHM-log.html', {
-		timeout : 1000, // 1000 ms
-		datatype:'html'
-	}).then(function(data){
-		var out_html = $($.parseHTML(data));//parse
-		$('#rate').empty().append(out_html.filter('#rate')[0].innerHTML);//insert
-	},function(jqXHR, textStatus) {
-		if(textStatus!=="success") {
-			var txt = "<b>textStatus:"+ textStatus + "</b>" +
-				"<b>status:"+ jqXHR.status + "</b>" +
-				"<b>responseText : </b><div>" + jqXHR.responseText +
-				"</div>";
-			$('#rate').html(txt);
-		}
-	});
-});
+var load_html_and_insert = function (html_url, insert_info_arr){
+    $.ajax(html_url, {
+        timeout : 1000,
+        datatype: 'html'
+    }).then(function(data){
+        var out_html = $($.parseHTML(data));//parse1
+        var i;
+        for(i = 0; i < insert_info_arr.length; ++i){
+            $("#" + insert_info_arr[i][1]).empty().append(out_html.filter("#" + insert_info_arr[i][0])[0].innerHTML);//insert
+        }
+    }, function(jqXHR, textStatus) {
+        if(textStatus!=="success") {
+            var txt = "<p>textStatus:"+ textStatus + "</p>" +
+                "<p>status:"+ jqXHR.status + "</p>" +
+                "<p>responseText : </p><div>" + jqXHR.responseText +
+                "</div>";
+            $('#page').html(txt);
+            $('#page2').html(txt);
+        }
+    });
+};
